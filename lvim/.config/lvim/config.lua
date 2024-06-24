611cf6ee7549ecbed3469cf1693c26c65a670ddb
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-06-24 01:06:35 -0700

    latest lvim config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -4,6 +4,14 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 --
+
+reload("user.colorscheme")
+reload("user.options")
+reload("user.which-key")
+reload("user.dashboard")
+reload("user.keymaps")
+reload("user.statusline")
+
 lvim.plugins = {
 	{
 		"folke/flash.nvim",
@@ -24,5 +32,105 @@ lvim.plugins = {
 		lazy = false,
 		priority = 1000,
 		opts = {},
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		event = "InsertEnter",
+		dependencies = { "zbirenbaum/copilot.lua" },
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+				require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+			end, 100)
+		end,
+	},
+	{ "ThePrimeagen/harpoon" },
+}
+
+function GrepInputString()
+	local default = vim.api.nvim_eval([[expand("<cword>")]])
+	local input = vim.fn.input({
+		prompt = "Search for: ",
+		default = default,
+	})
+	require("telescope.builtin").grep_string({ search = input })
+end
+
+lvim.builtin.which_key.mappings["sT"] = { "<cmd>lua GrepInputString()<CR>", "Text under cursor" }
+vim.opt.relativenumber = true
+
+local actions = require("telescope.actions")
+
+lvim.builtin.telescope = {
+	active = true,
+	on_config_done = nil,
+	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
+		prompt_prefix = "> ",
+		selection_caret = "> ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "descending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				mirror = false,
+			},
+			vertical = {
+				mirror = false,
+			},
+		},
+		file_sorter = require("telescope.sorters").get_fuzzy_file,
+		file_ignore_patterns = {},
+		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		path_display = {},
+		winblend = 0,
+		border = {},
+		borderchars = { "|", "|", "-", "-", "+", "+", "+", "+" },
+		color_devicons = true,
+		use_less = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		mappings = {
+			i = {
+				["<C-n>"] = actions.move_selection_next,
+				["<C-p>"] = actions.move_selection_previous,
+				["<C-c>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+			},
+			n = {
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+			},
+		},
 	},
 }
